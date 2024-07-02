4bc87edd9a8b9381c0214798e81b8fa14a966560
NithinAmbati <nithinambati9@gmail.com>
2024-07-02 13:33:14 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,77 +1,9 @@
-import { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import {
-  Grid,
-  Card,
-  Button,
-  Container,
-  CssBaseline,
-  Typography,
-  Toolbar,
-  AppBar,
-  Box,
-  IconButton,
-  MenuItem,
-  Menu,
-  MenuList,
-} from "@mui/material";
+import Header from "./components/Header.jsx";
 
 function App() {
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const openMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const closeMenu = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <>
-      <CssBaseline />
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
-            RGUKT Hospital
-          </Typography>
-
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Button color="inherit">Login</Button>
-            <Button color="inherit">Register</Button>
-          </Box>
-
-          <Box sx={{ display: { xs: "flex", md: "none" }, marginLeft: "auto" }}>
-            <IconButton
-              color="inherit"
-              size="large"
-              edge="end"
-              onClick={openMenu}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={closeMenu}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-            >
-              <MenuList>
-                <MenuItem onClick={closeMenu}>Home</MenuItem>
-                <MenuItem onClick={closeMenu}>About</MenuItem>
-                <MenuItem onClick={closeMenu}>Contact</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </AppBar>
+      <Header />
     </>
   );
 }

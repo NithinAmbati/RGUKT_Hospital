7a2c025b4b6e0e43845636fa71e9d859a1a088e2
NithinAmbati <nithinambati9@gmail.com>
2024-07-17 00:08:19 +0530

    Nursing Backend

--- backend/APIs/nursing.js
+++ backend/APIs/nursing.js
@@ -32,4 +32,20 @@ router.post("/", async (req, res) => {
   res.status(200).send("Successful");
 });
 
+router.put("/:treatmentId", async (req, res) => {
+  try {
+    const { spo2, ecg } = req.body;
+    console.log(req.body);
+    const { treatmentId } = req.params;
+    await Treatments.findByIdAndUpdate(
+      { _id: treatmentId },
+      { $set: { spo2, ecg } },
+      { new: true }
+    );
+    res.status(200).send("Successfully Updated");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
